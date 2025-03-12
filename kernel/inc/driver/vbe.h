7a616e9333308f1851885b38483dba65282200a7
zhai-dada <zhaipengwei0728@gmail.com>
2025-03-12 23:10:26 +0800

    增加VBE显示ASCII字符

--- kernel/inc/driver/vbe.h
+++ kernel/inc/driver/vbe.h
@@ -2,6 +2,19 @@
 #define __VBE_H__
 
 #include <stdint.h>
+#include <lib/libfont.h>
+
+// UEFI设置的分辨率为 1024 * 768
+
+#define WHITE   0x00ffffff
+#define BLACK   0x00000000
+#define RED     0x00ff0000
+#define ORANGE  0x00ff8000
+#define YELLOW  0x00ffff00
+#define GREEN   0x0000ff00
+#define BLUE    0x000000ff
+#define INDIGO  0x0000ffff
+#define PURPLE  0x008000ff
 
 typedef struct vbe_info
 {
@@ -17,5 +30,8 @@ typedef struct vbe_info
     u32 *vbe_base_addr;
     u64 vbe_buffer_length;
 } vbe_info_t;
+
+void vbe_init(void);
+s32 color_printk(u32 FRcolor, u32 BKcolor, const s8 *fmt, ...);
 
 #endif
