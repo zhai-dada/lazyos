d392d88f88754fc595e1a60707ee5189929a7540
zhai-dada <zhaipengwei0728@gmail.com>
2025-03-12 23:10:52 +0800

    操作显存，显示字符串

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,12 +1,16 @@
 #include <lib/string.h>
 #include <driver/serial.h>
+#include <driver/vbe.h>
+#include <uefi.h>
 
 int kernel(void)
 {
     char a[512] = {0};
     memcpy(a, "Hello\n", 7);
     serial_init();
+    vbe_init();
     serial_printf(SFGREEN, SBBLACK, "%s", a);
+    color_printk(YELLOW, BLACK, "%s\n", a);
     while(1)
     {
         ;
