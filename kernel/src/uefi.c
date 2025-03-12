20e01cd26b10e7d15ffb121bce295a49ab88c93b
zhai-dada <zhaipengwei0728@gmail.com>
2025-03-12 23:07:47 +0800

    增加UEFI启动参数地址

--- kernel/src/uefi.c
+++ kernel/src/uefi.c
@@ -1,3 +1,5 @@
 #include <uefi.h>
 
-struct KERNEL_BOOT_INFORMATION *boot_info;
+// UEFI保存所需数据的存放地址，具体参考UEFI代码
+struct KERNEL_BOOT_INFORMATION* boot_info = (struct KERNEL_BOOT_INFORMATION *)0xffff800000060000;
+
